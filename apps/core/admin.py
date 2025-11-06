ae5c18328f52d0006c1d0435f38fa33ae04cf095
Wesley Borges <djwesleyborges@gmail.com>
2025-11-06 17:08:13 -0300

    Implementado models.

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pet, Servico, Agendamento, ServicoRealizado
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'especie', 'raca', 'idade', 'tutor')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'preco', 'duracao_minutos')
+
+
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('pet', 'servico', 'data', 'status', 'observacao')
+
+
+@admin.register(ServicoRealizado)
+class ServicoRealizadoAdmin(admin.ModelAdmin):
+    list_display = ('agendamento', 'pet', 'servico', 'data_execucao',
+                    'observacao', 'avaliacao')
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    nome = models.CharField(max_length=100)
+    especie = models.CharField(max_length=100)
+    raca = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    duracao_minutos = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Agendamento(models.Model):
+    STATUS = (
+        ('Pendente', 'Pendente'),
+        ('Confirmado', 'Confirmado'),
+        ('Cancelado', 'Cancelado'),
+    )
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    data = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS, blank=False,
+                              null=False)
+    observacao = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.pet.nome
+
+
+class ServicoRealizado(models.Model):
+    agendamento = models.ForeignKey(Agendamento, on_delete=models.CASCADE)
+    data_execucao = models.DateTimeField()
+    observacao = models.TextField()
+    avaliacao = models.IntegerField()
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.agendamento.pet.nome
